d14c3c46cbfab554c6687397db07bf2fe9f3c6eb
Filip Anselmo <filip_anselmo@yahoo.com.br>
2019-10-21 11:49:03 -0400

    atualizando repositorio

--- rc4.py
+++ rc4.py
@@ -1,6 +1,4 @@
-from tkinter import *
-import socket
-from threading import Thread
+import binascii
 from random import *
 import random
 import sys
@@ -12,19 +10,19 @@ class RC4:
     chave = ""
 
     def __init__(self):
-        self.chave = self.convertKey()
+        self.chave = self.converteChave()
 
     def KSA(self):
-        self.p = 0
-        self.q = 0
+        #self.p = 0
+        #self.q = 0
         tamChave = len(self.chave)
-
+        
         self.S = range(256)
         j = 0
         for i in range(0,256):
-            j = (j + self.S[i] + ord(self.chave[i % tamChave])) % 256
+            j = (j + self.S[i] + self.chave[i % tamChave]) % 256
             self.S[i], self.S[j] = self.S[j], self.S[i]
-        
+    
     def PRGA(self):
         self.p = (self.p + 1) % 256
         self.q = (self.q + self.S[self.p]) % 256
@@ -35,7 +33,7 @@ class RC4:
     def encrypt(self, textoPlano):
         self.KSA()
         return "".join("%02X" % (ord(c) ^ self.PRGA()) for c in textoPlano)
-
+    
     def decrypt(self, cipher):
         self.KSA()
         listaByte = []
@@ -43,33 +41,20 @@ class RC4:
             byte = cipher[i:i+2]
             listaByte.append(int(byte, 16))
         return "".join([chr(byte ^ self.PRGA()) for byte in listaByte])
-    
-    def convertKey(self):
+
+    def converteChave(self):
         return[ord(c) for c in self.chave]
+    
 
 
-class Receive():
-    def __init__(self, servidor, messagesLog):
-        self.servidor = servidor
-        self.messagesLog = messagesLog
-
-        while(True):
-            try:
-                text = self.servidor.recv(1024)
-                
-
-
-'''
 if(__name__ == '__main__'):
     rc4 = RC4()
     chave = input('Sua chave: ')
-    rc4.convertKey()
+    rc4.converteChave()
     while(True):
         message = input('Sua mensagem: ')
         print(message)
         E = rc4.encrypt(message)
-
-        print("Mensagem cifrada: ", E)
+        print('Mensagem cifrada: ', E)
         M = rc4.decrypt(message)
-        print("Mensagem decifrada: ", M)
-'''
+        print('Mensagem decifrada: ', M)
